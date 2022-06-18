188c10ef9f105e3dcc9582e52f0c5b3125e7db5e
Christian Gabrielsson <christiangabrielsson@Christians-MacBook-Pro-2.local>
2022-06-18 15:10:25 -0700

    added material, flex, chartjs, ng2charts

--- angular-charts-app/src/app/app.module.ts
+++ angular-charts-app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgChartsModule} from "ng2-charts";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
